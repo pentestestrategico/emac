34522963b363ad81a0f5752ed86c68c20a934e05
Fabio <fabioluizrl2@gmail.com>
2025-07-05 00:14:39 -0300

    imagens inclusas

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
-import { CarrocelComponent } from '../carrocel/carrocel.component';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-body',
-  imports: [CarrocelComponent],
+  imports: [CommonModule],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+  styleUrls: ['./body.component.css'] 
 })
 export class BodyComponent {
+
+  fotos = [
+    {
+      texto: 'Autismo não é uma doença. É uma forma única de ver o mundo.',
+      imagem: '/assets/autismo.jpg'
+    },
+    {
+      texto: 'Empatia é o melhor caminho para a inclusão.',
+      imagem: '/assets/autismo1.jpg'
+    },
+    {
+      texto: 'Crianças autistas são como estrelas: cada uma brilha de um jeito único.',
+      imagem: '/assets/autismo5.jpg'
+    },
+    {
+      texto: 'Ser diferente é o que nos torna especiais.',
+      imagem: '/assets/menino2.jpeg'
+    },
+    {
+      texto: 'Pessoas autistas têm talentos incríveis — só precisam de espaço para brilhar.',
+      imagem: '/assets/autismo6.jpeg'
+    },
+    {
+      texto: 'O silêncio também é uma forma de comunicação.',
+      imagem: '/assets/autismo7.jpg'
+    },
+    {
+      texto: 'Não tente mudar quem sou. Me aceite como sou.',
+      imagem: '/assets/autismo8.jpg'
+    },
+    {
+      texto: 'Não nos encaixamos? Então vamos reinventar o molde.',
+      imagem: '/assets/autismo9.png'
+    },
+    {
+      texto: 'Autismo não precisa de pena. Precisa de oportunidade.',
+      imagem: '/assets/logocompleto.jpeg'
+    }
+  ];
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CarrocelComponent } from "../../components/carrocel/carrocel.component";
-
+import { BodyComponent } from '../../components/body/body.component';
 @Component({
   selector: 'app-home',
-  imports: [CarrocelComponent],
+  imports: [CarrocelComponent, BodyComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
