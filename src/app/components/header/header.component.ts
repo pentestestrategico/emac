ef11d737156eca00ebf7405233fcb019d199bdde
Fabio <fabioluizrl2@gmail.com>
2025-07-09 13:33:56 -0300

    atualizado firebase 13:30

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],//importar routermodule para os links
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule]
 })
 export class HeaderComponent {
-
+  // Código simplificado sem menu hambúrguer
 }
